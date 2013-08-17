770e216927f9a3b2ea7081ae2fcfc1917979a058
unknown <quynhlt@qsq-PC.qsoft.com.vn>
2013-08-17 10:37:17 +0700

    red

--- src/com/example/tactictoe/unitest/GameUltil.java
+++ src/com/example/tactictoe/unitest/GameUltil.java
@@ -5,7 +5,6 @@ import com.example.tactictoe.Gamer;
 public class GameUltil {
 
 	public static int checkResult(Gamer gamer) {
-		return 0;
+		return 1;
 	}
-
 }
--- test/com/example/tactictoe/unitest/GameUltilTest.java
+++ test/com/example/tactictoe/unitest/GameUltilTest.java
@@ -14,6 +14,7 @@ import com.example.tactictoe.Gamer;
 public class GameUltilTest extends TestCase {
 
 	private static final int X_WIN = 1;
+	private static final int Y_WIN = 2;
 
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -29,4 +30,13 @@ public class GameUltilTest extends TestCase {
 		assertTrue(result == X_WIN);
 	}
 
+	public void testEndGameWithYIsWinner() {
+		Gamer gamer = new Gamer();
+		String[] values = { "O", "X", "O", "X", "O" };
+		int[] moves = { 0, 3, 1, 4, 2 };
+		gamer.setValues(values);
+		gamer.setMoves(moves);
+		int result = GameUltil.checkResult(gamer);
+		assertTrue(result == Y_WIN);
+	}
 }
